4aec3356a0577eda6a320eb38b19291c20150f70
Jackson Henry <jacksonhenry3@gmail.com>
2016-08-25 14:18:08 -0700

    search working

--- js/gotorss.js
+++ js/gotorss.js
@@ -1,14 +1,24 @@
 $(document).keypress(function(e) {
-    if(e.which == 13) {
-    	querry = document.getElementById("rssInput").value.replace(/ /g,"+");
-        querry = querry.concat("&entity=podcast")
-        querry = "https://itunes.apple.com/search?term=".concat(querry)
-        $.getJSON(querry, function(data) {
-    	window.location.href = "podcast.html#"+data.results[0].feedUrl
+	if(e.which == 13) {
+		querry = document.getElementById("rssInput").value.replace(/ /g,"+");
+		querry = querry.concat("&entity=podcast")
+		querry = "https://itunes.apple.com/search?term=".concat(querry)
+		querry = querry.concat("&callback=goToPodcast")
+		console.log(querry)
+		$.getJSON(querry, function(data) {
+		window.location.href = "podcast.html#"+data.results[0].feedUrl
 		});
-        
-    }
+
+		bObj = new JSONscriptRequest(querry);
+		bObj.buildScriptTag();
+		bObj.addScriptTag();
+		
+	}
 });
 
 
 
+
+function goToPodcast(data) {
+	window.location.href = "podcast.html#"+data.results[0].feedUrl
+}
