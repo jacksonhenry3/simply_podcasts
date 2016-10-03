a785f344accc1aed305dd9412dc3e9f28c82f97a
Jackson Henry <jacksonhenry3@gmail.com>
2016-10-02 23:21:11 -0400

    search page baics

--- js/podcastObject.js
+++ js/podcastObject.js
@@ -91,61 +91,37 @@ function podcastSearchList(arg)
 	console.log(arg)
 }
 
-function createCORSRequest(method, url) {
-  var xhr = new XMLHttpRequest();
-  if ("withCredentials" in xhr) {
-
-    // Check if the XMLHttpRequest object has a "withCredentials" property.
-    // "withCredentials" only exists on XMLHTTPRequest2 objects.
-    xhr.open(method, url, true);
-
-  } else if (typeof XDomainRequest != "undefined") {
-
-    // Otherwise, check if XDomainRequest.
-    // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
-    xhr = new XDomainRequest();
-    xhr.open(method, url);
-
-  } else {
-
-    // Otherwise, CORS is not supported by the browser.
-    xhr = null;
-
-  }
-  return xhr;
-}
-
-var xhr
-
-$(document).keypress(function(e) {
-	if(e.which == 13) {
+$(document).keypress(function(e)
+{
+	if(e.which == 13)
+	{
 		querry = document.getElementById("rssInput").value.replace(/ /g,"+");
 		querry = querry.concat("&entity=podcast")
 		querry = "http://itunes.apple.com/search?term=".concat(querry)
 		querry = querry.concat("&callback=goToPodcast")
-		console.log(querry)
-		$.ajax({
-  url: querry,
-  dataType: "jsonp"
-},function(){console.log(data)});
-		// console.log("creating CORS request")
-		// xhr = createCORSRequest("GET",querry)
-		// console.log("Request loading")
-		// xhr.onloadstart = function(){console.log("BUTS")}
-		// xhr.onload = function()
-		// {	
-		// 	console.log("LOADED")
-  //   		var text = xhr.responseText;
-  //   		var title = getTitle(text);
-  //  			alert('Response from CORS request to ' + url + ': ' + title);
-  // 		};
-		// $.getJSON(querry)//, function(data) {
-		//window.location.href = "podcast.html#"+data.results[0].feedUrl
-		//});
 
-		// bObj = new JSONscriptRequest(querry);
-		// bObj.buildScriptTag();
-		// bObj.addScriptTag();
-		
+		request = $.ajax({url: querry, dataType: "jsonp"});
+
+		var list = document.createElement('ol');
+		list.id = 'songs'
+
+		request.done(function(data)
+		{
+			results = data.results
+			for (var i = 0; i <= results.length-1; i++)
+			{
+				if (i ==0)
+				{
+					console.log(results[i])
+				}
+				console.log(results[i])//.trackName
+				var item = document.createElement('li');
+				item.appendChild(document.createTextNode(results[i].trackName));
+				list.appendChild(item);
+			}
+
+			// $("#searchContainer").html('')
+			$("#searchContainer").append(list)
+		})
 	}
 });
